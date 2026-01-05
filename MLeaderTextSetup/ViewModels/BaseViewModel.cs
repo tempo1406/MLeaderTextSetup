b2850a26f74b62ef2646639eafdfd9df178d22a2
gakfc1406 <gakfc1406@gmail.com>
2026-01-05 12:43:14 +0700

    feat: add MVVM base classes

---
+++ MLeaderTextSetup/ViewModels/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MLeaderTextSetup.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
