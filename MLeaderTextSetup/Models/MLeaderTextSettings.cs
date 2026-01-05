17675984d9082f35bf4ee66afb90285647a41eb6
gakfc1406 <gakfc1406@gmail.com>
2026-01-05 12:42:43 +0700

    feat: add core models for MLeader text settings and preview data

---
+++ MLeaderTextSetup/Models/MLeaderTextSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLeaderTextSetup.Models
+{
+    public class MLeaderTextSettings
+    {
+        public string TextStyleName { get; set; } = "Standard";
+        public double TextHeight { get; set; } = 2.5;
+
+        public bool ColorByLayer { get; set; } = true;
+        public short ColorIndex { get; set; } = 4;
+
+        public string FormatTemplate { get; set; } = "{A}-{B},{N},{E}-{T} / d{D}";
+    }
+}
---
+++ MLeaderTextSetup/Models/PreviewData.cs
@@ -0,0 +1,14 @@
+﻿namespace MLeaderTextSetup.Models
+{
+    public class PreviewData
+    {
+        public string A { get; set; } = "1";
+        public string B { get; set; } = "11";
+        public string N { get; set; } = "1n";
+        public string E { get; set; } = "1e";
+        public string T { get; set; } = "2.5";
+        public string D { get; set; } = "20";
+
+        public string Custom { get; set; } = "CUSTOM";
+    }
+}
