0132ce4daa9b437714650603c9da4e07091bf15b
gakfc1406 <gakfc1406@gmail.com>
2026-01-05 13:38:36 +0700

    chore:  Uncomment and implement PreviewActions.BuildText method

--- MLeaderTextSetup/Actions/PreviewActions.cs
+++ MLeaderTextSetup/Actions/PreviewActions.cs
@@ -1,21 +1,21 @@
-﻿//using MLeaderTextSetup.Models;
+﻿using MLeaderTextSetup.Models;
 
-//namespace MLeaderTextSetup.Actions
-//{
-//    internal class PreviewActions
-//    {
-//        public static string BuildText(MLeaderTextSettings s, PreviewData d)
-//        {
-//            string text = s.FormatTemplate
-//                .Replace("{A}", d.A)
-//                .Replace("{B}", d.B)
-//                .Replace("{N}", d.N)
-//                .Replace("{E}", d.E)
-//                .Replace("{T}", d.T)
-//                .Replace("{D}", d.D)
-//                .Replace("{CUSTOM}", d.Custom);
+namespace MLeaderTextSetup.Actions
+{
+    public class PreviewActions
+    {
+        public static string BuildText(MLeaderTextSettings s, PreviewData d)
+        {
+            string text = s.FormatTemplate
+                .Replace("{A}", d.A)
+                .Replace("{B}", d.B)
+                .Replace("{N}", d.N)
+                .Replace("{E}", d.E)
+                .Replace("{T}", d.T)
+                .Replace("{D}", d.D)
+                .Replace("{CUSTOM}", d.Custom);
 
-//            return text;
-//        }
-//    }
-//}
+            return text;
+        }
+    }
+}
