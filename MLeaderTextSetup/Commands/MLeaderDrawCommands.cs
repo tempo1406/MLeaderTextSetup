9723715a84cd54032fd89dd7560763ada29c3b43
gakfc1406 <gakfc1406@gmail.com>
2026-01-06 22:17:12 +0700

    refactor: remove unused commands

--- MLeaderTextSetup/Commands/MLeaderDrawCommands.cs
+++ MLeaderTextSetup/Commands/MLeaderDrawCommands.cs
@@ -1,42 +1,15 @@
 using Autodesk.AutoCAD.Runtime;
 using MLeaderTextSetup.Actions;
-using MLeaderTextSetup.Models;
 
 namespace MLeaderTextSetup.Commands
 {
     public class MLeaderDrawCommands
     {
-        private MLeaderTextSettings GetSettingsFromBridge()
-        {
-            return new MLeaderTextSettings
-            {
-                TextStyleName = DrawDataBridge.TextStyleName,
-                TextHeight = DrawDataBridge.TextHeight,
-                ColorIndex = DrawDataBridge.ColorIndex,
-                ColorByLayer = DrawDataBridge.ColorByLayer,
-                FormatTemplate = DrawDataBridge.FormatTemplate
-            };
-        }
-
         [CommandMethod("MLEADER_DRAW")]
         public void Draw()
         {
-            var settings = GetSettingsFromBridge();
+            var settings = SettingsActions.LoadFromDrawing() ?? new Models.MLeaderTextSettings();
             MLeaderActions.CreateNewMLeader(settings);
-        }
-
-        [CommandMethod("MLEADER_MULTI_POINT")]
-        public void MultiPoint()
-        {
-            var settings = GetSettingsFromBridge();
-            MLeaderActions.CreateMLeaderWithMultiplePoints(settings);
-        }
-
-        [CommandMethod("MLEADER_MULTI_DRAW")]
-        public void MultiDraw()
-        {
-            var settings = GetSettingsFromBridge();
-            MLeaderActions.CreateMultipleMLeaders(settings);
         }
     }
 }
